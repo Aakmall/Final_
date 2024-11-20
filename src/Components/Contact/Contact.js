4665b3de63d4f50ad035f2fffe1964984d9c9403
Wensynovansompa <s11910215@student.unklab.ac.id>
2024-11-20 23:27:27 +0800

    tambahan Firebase untuk contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -88,11 +88,11 @@ const Contact = () => {
           </div>
           <footer className="absolute bottom-0 left-0 w-full">
             <div className="text-white container mx-auto tm-container p-8 text-lg flex flex-col md:flex-row justify-between">
-              <span>Copyright 2022 Antique Cafe. All rights reserved.</span>
+              <span>{contact.contact9}.</span>
               <span className="mt-5 md:mt-0">
-                Design:{" "}
+                {contact.contact10}{" "}
                 <a href="https://www.tooplate.com" target="_parent">
-                  Tooplate
+                  {contact.contact11}
                 </a>
               </span>
             </div>
