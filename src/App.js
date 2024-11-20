2ff4510ad21817ca3e35816b8cb9ced5491434ee
KharNev <s22210658@student.unklab.ac.id>
2024-11-20 16:10:53 +0800

    Copied all files from <branch-sumber>

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Intro from "./Components/Intro/Intro.js";
+import Menu from "./Components/Menu/Menu.js";
+import About from "./Components/About/About.js";
+import "./App(import)";
+
+const App = () => {
+  return (
+    // parent
+    <div>
+      {/* intro */}
+      <Intro />
+      {/* menu */}
+      <Menu />
+      {/* about */}
+      <About />
+      {/* contact */}
+    </div>
+  );
+};
+
+export default App;
