4928a86744553f3c44d36e916531a71506c898b7
yesa2711 <yesamokolensang@gmail.com>
2024-11-20 09:47:33 +0800

    change

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Contact() {
+const Contact = () => {
   return (
     <div>
       <a
@@ -104,5 +104,5 @@ function Contact() {
       </div>
     </div>
   );
-}
+};
 export default Contact;
